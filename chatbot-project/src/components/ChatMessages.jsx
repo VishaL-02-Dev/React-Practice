b5fc35df70a19b6e4d838eb840f910663c7a80f3
vishal <vishalvinod02@gmail.com>
2026-01-15 21:09:59 +0530

    Final commit

--- chatbot-project/src/components/ChatMessages.jsx
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -1,28 +1,29 @@
-import{ useRef, useEffect } from 'react'
+import { useRef, useEffect } from 'react'
+import { ChatMessage } from '../components/ChatMessage'
 
 export function ChatMessages({ chatMessages }) {
-  const chatMessagesRef = useRef(null)
+    const chatMessagesRef = useRef(null)
 
-  useEffect(() => {
-    const containerElem = chatMessagesRef.current;
-    if (containerElem) {
-      containerElem.scrollTop = containerElem.scrollHeight;
-    }
-    console.log('updated');
-  }, [chatMessages])
+    useEffect(() => {
+        const containerElem = chatMessagesRef.current;
+        if (containerElem) {
+            containerElem.scrollTop = containerElem.scrollHeight;
+        }
+        console.log('updated');
+    }, [chatMessages])
 
-  return (
-    <div className="chat-msg-container" ref={chatMessagesRef}>
-      {chatMessages.map((chatMessage) => {
-        return (
-          <ChatMessage
-            msg={chatMessage.msg}
-            sender={chatMessage.sender}
-            key={chatMessage.id}
-          />
-        )
-      })}
-    </div>
-  );
+    return (
+        <div className="chat-msg-container" ref={chatMessagesRef}>
+            {chatMessages.map((chatMessage) => {
+                return (
+                    <ChatMessage
+                        msg={chatMessage.msg}
+                        sender={chatMessage.sender}
+                        key={chatMessage.id}
+                    />
+                )
+            })}
+        </div>
+    );
 
 }
