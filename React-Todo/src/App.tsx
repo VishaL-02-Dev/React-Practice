d8e1d52638db6f0542f7c450e2691287075bbf49
vishal <vishalvinod02@gmail.com>
2026-01-21 01:34:00 +0530

    Added validation, remove duplicates and edit todo

--- React-Todo/src/App.tsx
+++ React-Todo/src/App.tsx
@@ -9,7 +9,20 @@ function App(){
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodo = (text: string)=>{
-    if(!text.trim()) return;
+    const trimmedText = text.trim();
+
+    if(!trimmedText){
+      alert("Todo cannot be empty");
+      return;
+    }
+
+    const isDuplicate = todos.some(
+      todo => todo.text.toLowerCase() === trimmedText.toLocaleLowerCase()
+    );
+    
+    if(isDuplicate){
+      alert("Todo already exists!")
+    }
 
     setTodos([
       ...todos,
--- React-Todo/src/components/TodoItem.tsx
+++ React-Todo/src/components/TodoItem.tsx
@@ -1,27 +1,58 @@
 import type { Todo } from '../types/todo'
+import type { useState } from 'react'
 
-interface TodoItemProps{
+interface TodoItemProps {
     todo: Todo;
-    onToggle: (id: string)=> void;
-    onDelete: (id: string)=> void;
+    onToggle: (id: string) => void;
+    onDelete: (id: string) => void;
+    onEdit: (id: stirng) => void;
 }
 
-export function TodoItem({todo, onToggle, onDelete}: TodoItemProps){
-    return(
+export function TodoItem({ todo, onToggle, onDelete, onEdit }: TodoItemProps) {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editText, setEditText] = useState(todo.text);
+
+    const handleSave = () => {
+        if (!editText.trim()) {
+            alert("Todo cannot be empty");
+            return;
+        }
+
+        onEdit(todo.id, editText.trim());
+        setIsEditing(false);
+    };
+
+    return (
         <li className='todo-item'>
             <input
                 type="checkbox"
                 checked={todo.completed}
-                onChange={()=>onToggle(todo.id)}
+                onChange={() => onToggle(todo.id)}
             />
-            <span
-                style={{
-                    textDecoration: todo.completed ? "line-through" : "none",
-                }}
-            >
-                {todo.text}
-            </span>
-            <button onClick={()=> onDelete(todo.id)}>❌</button>
+
+            {isEditing ? (
+                <input
+                    value={editText}
+                    onChange={(e) => setEditText(e.target.value)}
+                />
+            ) : (
+                <span
+                    style={{
+                        textDecoration: todo.completed ? "line-through" : "none",
+                    }}
+                >
+                    {todo.text}
+                </span>
+            )}
+
+            {isEditing ? (
+                <button onClick={handleSave}>💾</button>
+            ) : (
+                <button onClick={() => setIsEditing(true)}>✏️</button>
+            )}
+
+            <button onClick={() => onDelete(todo.id)}>❌</button>
         </li>
     );
 }
